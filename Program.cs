aed108c0c88a0ff180ac9172de3da80d3de6a05d
Xabier Figueroa <xabierfigueroa96@gmail.com>
2026-02-06 15:35:20 +0100

    Enhance Swagger configuration with detailed API documentation and XML comments

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using System.Reflection;
 using TflBackendService.Clients;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "TfL Backend Service",
+        Version = "v1",
+        Description = "Backend service that consumes and normalizes data from the Transport for London (TfL) Open Data API."
+    });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<TflApiClient>(client =>
